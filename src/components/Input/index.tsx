9f0d92d5a3936c22179930b3a14b4f88006ba898
eduardo.reichert <edrdesigner@gmail.com>
2020-05-15 23:30:17 -0300

    first commit

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, TextInput } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon?: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
+  <>
+    <Container>
+      <TextInput
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        {...rest}
+      />
+    </Container>
+  </>
+);
+
+export default Input;
