e83648908b4bba9bf734b36e4bcd8df99c35a82f
eduardo.reichert <edrdesigner@gmail.com>
2020-08-30 17:44:21 -0300

    chore: criando pagina de rotas

---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CreateAppointment: React.FC = () => {
+  return <View style={{ flex: 1, justifyContent: 'center' }} />;
+};
+
+export default CreateAppointment;
